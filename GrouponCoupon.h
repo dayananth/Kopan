c2e0bd10241c650f6b69d24f3e4090da89ea95a9
nseenivasan <nseenivasan@groupon.com>
2015-11-30 22:30:20 -0800

    Completed account page

---
+++ GrouponCoupon.h
@@ -0,0 +1,26 @@
+//
+//  GrouponCoupon.h
+//  Kopan
+//
+//  Created by Nizha Shree Seenivasan on 11/28/15.
+//  Copyright © 2015 Nizha Shree Seenivasan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "GrouponDeal.h"
+
+
+@class MyAccountTableViewCell;
+@interface GrouponCoupon : NSObject
+
+@property (strong, nonatomic) NSDictionary* coupon;
+@property (strong, nonatomic) GrouponDeal* deal;
+@property (strong, nonatomic) NSString* couponCode;
+@property (strong, nonatomic) NSString* source;
+@property (strong, nonatomic) NSString* request_to_sell;
+@property (nonatomic) int user_id;
+@property (strong, nonatomic) NSDate* expiresAt;
+@property (strong, nonatomic) MyAccountTableViewCell* myAccountTableViewCell;
+
+- (id) initWithDictionary:(NSDictionary*) dictionary;
+@end
